43dd7e0c0b3789aa05ead8496e65bfc532bfb26a
Swatijoshi2002 <79635662+Swatijoshi2002@users.noreply.github.com>
2022-02-20 22:01:05 +0530

    Create validparenthesis.cpp

---
+++ validparenthesis.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(st.size()==0)
+            {
+                st.push(s[i]);
+            }
+           else if(st.top()=='(')
+            {
+                 if(s[i]==')')
+                     st.pop();
+                else st.push(s[i]);
+            }
+           else if(st.top()=='[')
+            {
+                 if(s[i]==']')
+                     st.pop();
+                else st.push(s[i]);
+            }
+           else if(st.top()=='{')
+            {
+                 if(s[i]=='}')
+                     st.pop();
+                else st.push(s[i]);
+            }
+        }
+        if(st.size()==0)
+            return true;
+        else return false;
+    }
+};
