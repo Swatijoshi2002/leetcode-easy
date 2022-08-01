fd8727be302116bde8708559eb0197426d116545
Swatijoshi2002 <79635662+Swatijoshi2002@users.noreply.github.com>
2022-08-01 16:48:28 +0530

    Create minDEPTHway2.cpp

---
+++ minDEPTHway2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  
+    int minDepth(TreeNode* root) {
+       if(root==NULL)
+           return 0;
+        if(!root->left && !root->right)
+            return 1;
+        if(!root->left)
+            return 1+minDepth(root->right);
+          if(!root->right)
+            return 1+minDepth(root->left);
+        else return 1+min(minDepth(root->right),minDepth(root->left));
+        
+    }
+};
